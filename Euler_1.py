bb6557fec04d9221055455094f8e9c3a097cb4f0
sjwalters <particlespin@yahoo.com>
2017-02-13 07:47:30 -0600

    Comments Added to Euler_1.py
    Comments were added to give the problem description and a description of my solution

--- Euler_1.py
+++ Euler_1.py
@@ -1,3 +1,10 @@
+# Problem #1:  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+# The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+# My solution allows a user to input an integer
+# then calculates the sum of the multiples of 3 or 5 below that user input
+
 total = 0
 upper_bound = int(input('Enter number: '))
 
